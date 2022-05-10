b95c1b0f78850292879065a525752abf5f2668f8
Michele Ascoli <micheleascoli17@gmail.com>
2022-05-10 16:30:43 -0500

    mapeando as minhas entidades com JPA

--- src/main/java/com/dio/santander/bankline/api/model/Correntista.java
+++ src/main/java/com/dio/santander/bankline/api/model/Correntista.java
@@ -1,9 +1,34 @@
 package com.dio.santander.bankline.api.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "tab_correntista")
 public class Correntista {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(length = 20)
 	private String cpf;
+	
+	@Column(length = 60)
 	private String nome;
+	
+	/*
+	 *  A anotação @Embedded cria uma relacao entre tabelas, isto é, 
+	 *  as informações que estiverem nessa classe, estarã na mesma tabela
+	 *  que a tabela principal, a tabela que possui uma referencia a essa classe.
+	 *  */
+
+	@Embedded
 	private Conta conta;
 	public Conta getConta() {
 		return conta;
